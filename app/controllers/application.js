eb7f09b991966864e9ce59e35c3cf9bfdb4d367f
Laszlo Bacsi <lackac@lackac.hu>
2013-10-01 01:08:20 +0200

    Navigate to previous or next chapter with pager links

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,27 @@
 var ApplicationController = Ember.Controller.extend({
   ref: Reference.resolve('ujforditas', '1Móz 1'),
 
-  abbr: function(key, value, oldValue) {
+  abbr: function() {
     return this.get('ref').abbr;
   }.property('ref'),
+
+  previousChapterPath: function() {
+    var prev = this.get('ref').previousChapter();
+    return prev && prev.path;
+  }.property('ref'),
+
+  noPreviousChapter: function() {
+    return !this.get('previousChapterPath');
+  }.property('previousChapterPath'),
+
+  nextChapterPath: function() {
+    var next = this.get('ref').nextChapter();
+    return next && next.path;
+  }.property('ref'),
+
+  noNextChapter: function() {
+    return !this.get('nextChapterPath');
+  }.property('nextChapterPath'),
 
   actions: {
     query: function(value) {
@@ -18,6 +36,16 @@ var ApplicationController = Ember.Controller.extend({
         // reset to original abbr
         this.notifyPropertyChange('ref');
       }
+    },
+
+    previousChapter: function() {
+      var path = this.get('previousChapterPath');
+      if (path) this.transitionToRoute(path);
+    },
+
+    nextChapter: function() {
+      var path = this.get('nextChapterPath');
+      if (path) this.transitionToRoute(path);
     }
   }
 });
